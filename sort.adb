8ff0edcbbe920f8e425301033570f6c032d7cb45
pratheeksh <pks329@nyu.edu>
2016-02-26 11:59:04 -0500

    working sort code

--- sort.adb
+++ sort.adb
@@ -10,81 +10,72 @@ procedure Main is
 		a := b;
 		b := temp;
 	end Swap;
-	task partition is
-		entry startPart(lo: integer; hi: integer);
-	end partition;
-	task quicksortTask1 is
-		entry startSort(lo: integer; hi: integer);
-	end quicksortTask1;
-		task quicksortTask2 is
-		entry startSort(lo: integer; hi: integer);
-	end quicksortTask2;
+	procedure quicksort(lo,hi:integer ) is
 	
-	task body  partition is
-		i : integer;
-		pivot: integer;
-		j: integer;
-		low: integer;
-		high: integer;
-	begin
-		accept startPart(lo: integer; hi: integer) do
-			pivot := A(hi);
-			i := lo;
-			j := lo;
-			low := lo;
-			high := hi;
-			while j <= hi-1 loop
-				if A(j) < pivot then
-					swap(A(i), A(j));
-					i := i+1;
-				end if;
-				j := j+1;
-			end loop;
-			swap(A(i),A(j));
-			for i in 1..30 loop
-				put (A(i));
-			new_line;
-			end loop;
-			new_line;
- 		end startPart;
-			new_line;
+			pivot: integer;
+		function partition(lo,hi: integer) return integer is
+			i : integer;
+			j: integer;
+			low: integer;
+			high: integer;
+		begin
+				pivot := A(hi);
+				i := lo;
+				j := lo;
+				low := lo;
+				high := hi;
+				while j <= hi-1 loop
+					if A(j) < pivot then
+						swap(A(i), A(j));
+						i := i+1;
+					end if;
+					j := j+1;
+				end loop;
+				swap(A(i),A(j));
+				for i in 1..30 loop
+					put (A(i));
+				new_line;
+				end loop;
+		return i;
 			--put(low, i, high);
-			new_line;
-			quicksortTask1.startSort(low,i-1);
-			quicksortTask2.startSort(i+1,high);
-	end partition;
-    task body quicksortTask1 is
-		low: integer;
-		high: integer;
+		end;
+		procedure go(lo,pivot,hi: integer) is
+			low: integer;
+			high: integer;
+			task quicksortTask1;
+			task quicksortTask2;
+			task body quicksortTask1 is
+			begin
+				low := lo;
+				high := hi;
+			
+			if low < high then
+				quicksort(low,pivot-1);
+			end if;
+			end quicksortTask1;
+			task body quicksortTask2 is
+			begin
+				low := lo;
+				high := hi;
+			
+			if low < high then
+				quicksort(pivot+1,high);
+			end if;
+			end quicksortTask2;
 		begin
-		accept startSort(lo:Integer; hi:Integer) do
-			low := lo;
-			high := hi;
-		end startSort;
-		
-		if low < high then
-			partition.startPart(low,high);
-		end if;
-	end quicksortTask1;
-		task body quicksortTask2 is
-		low: integer;
-		high: integer;
-		begin
-		accept startSort(lo:Integer; hi:Integer) do
-			low := lo;
-			high := hi;
-		end startSort;
-		
-		if low < high then
-			partition.startPart(low,high);
-		end if;
-	end quicksortTask2;
-		
+			null;
+		end go;
+	begin
+		pivot := partition(lo,hi);
+		go(lo,pivot,hi);
+	end quicksort;
+
+	
 begin
 	for i in 1..30 loop
 		get(A(i));
 	end loop;
-	partition.startPart(1,30);
+	quicksort(1,30);
 	for i in 1..30 loop
 		put(A(i));
 	end loop;
